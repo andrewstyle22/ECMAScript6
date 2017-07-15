99cd1ffb3fd0686441a88aae10be9a63ddeed521
andrewstyle22 <andrewstyle22@yahoo.es>
2017-07-15 22:49:28 +0100

    subir 1º vez

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,4 +1,74 @@
-'use strict';
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     var monthlyRate = 0;
@@ -17,7 +87,7 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
 
     var balance = principal;
     var amortization = [];
-    var numerYear = 0;
+    var numberYear = 0;
 
     for (var y = 0; y < years; y++) {
         var interestY = 0; //Interest payment for year y
@@ -31,8 +101,8 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
             principalY = principalY + principalM;
             balance = balance - principalM;
         }
-        numerYear++;
-        amortization.push({ principalY: principalY, interestY: interestY, balance: balance, numerYear: numerYear });
+        numberYear++;
+        amortization.push({ principalY: principalY, interestY: interestY, balance: balance, numberYear: numberYear });
     }
 
     return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
@@ -71,3 +141,7 @@ document.getElementById('calcBtn').addEventListener('click', function () {
         return console.log(month);
     });
 });
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.bundle.js.map
